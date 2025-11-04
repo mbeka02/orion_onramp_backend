cf68f7082bfa169438cbcf3492517ccebd39e52b
Roman Njoroge <roman.njoroge@njuguna.com>
2025-11-04 16:26:39 +0300

    feat: Setting up table for storing environments

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,1 +1,14 @@
+import { text } from "drizzle-orm/pg-core";
+import { pgTable, pgEnum } from "drizzle-orm/pg-core";
+import { ENVIRONMENT_TYPES } from "../../types/environments";
+
+export const environment = pgEnum("environment_types", [ENVIRONMENT_TYPES.LIVE, ENVIRONMENT_TYPES.TEST])
+
 // Put database schemas here
+export const environments = pgTable("environments", {
+    publicKey: text("public_key").notNull().unique(),
+    privateKey: text("private_key").notNull().unique(),
+    type: environment().notNull(),
+    webhookUrl: text("webhook_url"),
+    callbackUrl: text("callback_url")
+})
---
+++ src/types/environments.ts
@@ -0,0 +1,4 @@
+export enum ENVIRONMENT_TYPES {
+    TEST = "test",
+    LIVE = "live"
+}

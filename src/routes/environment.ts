fcaaa3de9e4119200149093683b900ac81456f0d
Roman Njoroge <roman.njoroge@njuguna.com>
2025-11-06 14:37:38 +0300

    feat: Route for creating environment

--- src/routes/environment.ts
+++ src/routes/environment.ts
@@ -1,13 +1,18 @@
 import Express, { Router } from "express";
 import logger from "../lib/logger";
 import { createEnvironmentSchema } from "../types/environments";
+import environmentController from "../controllers/environments";
+import environmentModel from "../models/environments";
+import { authenticationMiddleware } from "../middleware/authenticationMiddleware";
 const router: Router = Express.Router();
 
-router.post("/", async(req, res) => {
+router.post("/", authenticationMiddleware, async(req, res) => {
     try {
         const parsed = createEnvironmentSchema.safeParse(req.body);
         if (parsed.success) {
             const data = parsed.data;
+            // TODO: Add code for getting business id from request
+            await environmentController.create(data, "", environmentModel);
         } else {
             const error = parsed.error.issues[0].message;
             logger.error("Environment Route: Invalid create data", {data: req.body, error});

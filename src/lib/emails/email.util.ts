e7e49e94c047d638dda7e0d57c5a50a4d77c5cb9
Patrick Ojiambo <pashrick237@gmail.com>
2025-11-08 18:42:54 +0300

    feat: implement email verification functionality

--- src/lib/emails/email.util.ts
+++ src/lib/emails/email.util.ts
@@ -1,12 +1,23 @@
 import { sendEmail } from './emailSender';
 import ExampleEmail from './templates/example';
+import VerifyEmail from "./templates/verify-email"
 export class EmailService {
-    async testEmail(){
+    async testEmail() {
         await sendEmail({
             to: "pashrick237@gmail.com", //Change as you guys test
             subject: "Test Email setup",
             react: ExampleEmail()
         })
+    }
+    async verificationEmail(url: string, to: string) {
+        if (typeof (url) !== 'string' || !to) {
+            throw new Error("Invalid arguments for verification email");
+        }
+        await sendEmail({
+            to,
+            subject: "Verify your email address",
+            react: VerifyEmail({ url })
+        });
     }
 }
 

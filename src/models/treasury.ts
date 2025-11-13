0a1bdced85df570ef0c10a12332a438faeef9433
Roman Njoroge <roman.njoroge@njuguna.com>
2025-11-13 20:53:54 +0300

    refactor: Using new transactions table and transaction status enum

--- src/models/treasury.ts
+++ src/models/treasury.ts
@@ -1,17 +1,18 @@
-import { sql } from "drizzle-orm";
+import { and, eq, sql } from "drizzle-orm";
 import { db } from "../lib/db";
 import logger from "../lib/logger";
 import { transactionsTable } from "../lib/db/schema";
-import { TransactionStatus } from "../types/transactions";
+import { TRANSACTION_STATUS } from "../types/transactions";
 
 export class TreasuryModel {
     async doesTransactionExist(transaction_id: string): Promise<boolean> {
         try {
-            const transactionRows = await db.execute(sql`
-                SELECT ${transactionsTable.id} FROM ${transactionsTable} WHERE ${transactionsTable.id} = ${transaction_id} FOR SHARE
-            `);
+           const transactionRes = await db.select({
+                id: transactionsTable.id
+            }).from(transactionsTable)
+            .where(eq(transactionsTable.id, transaction_id));
 
-            return transactionRows.rowCount ? transactionRows.rowCount > 0 : false;
+            return transactionRes.length > 0;
         } catch(err) {
             logger.error("Error checking if transaction exists", {error: err, transaction_id});
             throw new Error("Could not check if transaction id exists");
@@ -21,11 +22,15 @@ export class TreasuryModel {
     // Assumes that a transaction with the given ID already exists
     async hasTransactionAlreadyBeenOnramped(transaction_id: string): Promise<boolean> {
         try {
-            const onrampedTransaction = await db.execute(sql`
-               SELECT ${transactionsTable.id} FROM ${transactionsTable} WHERE ${transactionsTable.id} = ${transaction_id} AND ${transactionsTable.transactionStatus} = ${TransactionStatus.ONRAMPED}
-            `);
+            const onrampTransactionsRes = await db.select({
+                id: transactionsTable.id
+            }).from(transactionsTable)
+            .where(and(
+                eq(transactionsTable.id, transaction_id),
+                eq(transactionsTable.transactionStatus, TRANSACTION_STATUS.ONRAMPED)
+            ));
 
-            return onrampedTransaction.rowCount ? onrampedTransaction.rowCount > 0 : false;
+            return onrampTransactionsRes.length > 0;
         } catch(err) {
             logger.error("Could not check if transaction has already been onramped in database", {error: err, transaction_id});
             throw new Error("Could not check if transaction has already been onramped");

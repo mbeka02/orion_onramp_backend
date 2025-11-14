c4a9ed51371f1c21a6a223ca4ca9296130553f71
Patrick Ojiambo <pashrick237@gmail.com>
2025-11-14 11:54:20 +0300

    feat: Extend Errors enum with additional business-related error messages

--- src/errors.ts
+++ src/errors.ts
@@ -10,5 +10,11 @@ export enum Errors {
   BUSINESS_ALREADY_HAS_ENVIRONMENT = "Business environment already created",
   BUSINESS_DOES_NOT_HAVE_ENVIRONMENT = "Business does not have environment",
   BUSINESS_DOES_NOT_HAVE_KEYS = "Business does not have keys",
-  INTERNAL_SERVER_ERROR = "Internal Server Error"
+  INTERNAL_SERVER_ERROR = "Internal Server Error",
+  BUSINESS_NOT_FOUND = "Business not found",
+  UNAUTHORIZED = "Unauthorized",
+  INVALID_BUSINESS_DATA = "Invalid business data",
+  BUSINESS_CREATION_FAILED = "Could not create business",
+  INVITATION_FAILED = "Could not create invitation",
+  REGISTRATION_NUMBER_TAKEN = "Business registration number already in use"
 }

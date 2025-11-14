f39a2d5f6fe63c0162a88bc00bf8233919052499
Roman Njoroge <roman.njoroge@njuguna.com>
2025-11-14 15:21:23 +0300

    feat: Function for getting treasury balance for account

--- src/models/liquidityManager.ts
+++ src/models/liquidityManager.ts
@@ -3,9 +3,11 @@ import { db } from "../lib/db";
 import { treasuryBalanceTable } from "../lib/db/schema";
 import logger from "../lib/logger";
 import { TOKEN_TYPE } from "../types/token";
+import { HederaChainModel } from "./chain/hedera";
+import { SUPPORTED_CHAINS } from "../types/chain";
 
 export class LiquidityManagerModel {
-    async getCachedTokenBalance(token_type: TOKEN_TYPE): Promise<number> {
+    async getCachedTreasuryTokenBalance(token_type: TOKEN_TYPE): Promise<number> {
         try {
             const tokenBalance = await db.select({
                 balance: treasuryBalanceTable.balance
@@ -20,6 +22,32 @@ export class LiquidityManagerModel {
         } catch(err) {
             logger.error("Error getting cached treasury balance", {error: err, token_type});
             throw new Error("Error getting cached treasury balance");
+        }
+    }
+
+    async getTreasuryBalanceFromOnchain(token_type: TOKEN_TYPE, hederaChainModel: HederaChainModel): Promise<number> {
+        try {
+            const tokenDetails = await db.select({
+                address: treasuryBalanceTable.address,
+                treasuryAccount: treasuryBalanceTable.treasuryAccount,
+                chain: treasuryBalanceTable.chain
+            }).from(treasuryBalanceTable)
+            .where(eq(treasuryBalanceTable.token, token_type));
+
+            if (tokenDetails.length < 1) {
+                return 0;
+            } else {
+                const tokenDetail = tokenDetails[0];
+                if (tokenDetail.chain === SUPPORTED_CHAINS.HEDERA_MAINNET || tokenDetail.chain === SUPPORTED_CHAINS.HEDERA_TESTNET) {
+                    const balance = await hederaChainModel.getTokenBalance(tokenDetail.chain, tokenDetail.treasuryAccount, tokenDetail.address);
+                    return balance;
+                }
+
+                return 0;
+            }
+        } catch(err) {
+            logger.error("Error getting token balance on chain", {error: err, token_type});
+            throw new Error("Error getting token balance on chain");
         }
     }
 }

754aead850a07540245acc3558498150ece9b0fc
Roman Njoroge <roman.njoroge@njuguna.com>
2025-11-14 16:04:16 +0300

    feat: Running cached treasury balance cron job
    Running cron job for updating cached treasury balance when running the server, by default it runs every hour. If this or anything fails there I've also added graceful shutdown logic for the server that will stop the treasury cached balance update job

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import cors from "cors";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
 import environmentRouter from "./routes/environment";
+import { Server } from "http";
+import { startCachedTreasuryBalanceUpdate, stopCachedTreasuryBalanceUpdates } from "./services/treasuryBalance";
 
 const PORT = process.env.PORT;
 const DATABASE_URL = process.env.DATABASE_URL;
@@ -31,6 +33,63 @@ app.get("/", async (req, res) => {
 });
 app.use("/api/environment", environmentRouter);
 
-app.listen(PORT, () => {
-  logger.info(`Server is running on port ${PORT}`);
+let server: Server;
+let isShuttingDown = false;
+// Start background job
+async function initialize(): Promise<void> {
+  try {
+    // Start background cached treasury balance update every hour
+    startCachedTreasuryBalanceUpdate("0 * * * *");
+    logger.info("Cached treasury balance updates initialized successfully");
+  } catch (err) {
+    logger.error(`Failed to initialize cached treasury balance update service:${err}`);
+    process.exit(1);
+  }
+}
+
+async function gracefulShutdown(signal: string): Promise<void> {
+  //prevent multiple shutdown calls
+  if (isShuttingDown) {
+    return;
+  }
+  logger.info(`${signal} received. Starting graceful shutdown...`);
+  isShuttingDown = true;
+  const forceShutdownTimeout = setTimeout(() => {
+    logger.error("Graceful shutdown timed out. Forcing shutdown...");
+    process.exit(1);
+  }, 10000); // 10 seconds timeout
+
+  try {
+    if (server) {
+      await new Promise<void>((resolve, reject) => {
+        server.close((err) => {
+          if (err) {
+            logger.error(`Error closing HTTP server:${err}`);
+            reject(err);
+          } else {
+            logger.info("HTTP server closed");
+            resolve();
+          }
+        });
+      });
+    }
+    stopCachedTreasuryBalanceUpdates();
+    logger.info("Background job stopped");
+
+    clearTimeout(forceShutdownTimeout);
+
+    logger.info("Graceful shutdown completed successfully");
+    process.exit(0);
+  } catch (err) {
+    logger.error(`Error during graceful shutdown:${err}`);
+    clearTimeout(forceShutdownTimeout);
+    process.exit(1);
+  }
+}
+
+process.on("SIGINT", () => gracefulShutdown("SIGINT"));
+initialize().then(() => {
+  server = app.listen(PORT, () => {
+    logger.info(`Server is running on port ${PORT}`);
+  });
 });

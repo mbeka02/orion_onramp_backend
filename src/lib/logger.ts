af80513042d5caf5835268a41f65f064140d1e08
Roman Njoroge <roman.njoroge@njuguna.com>
2025-11-17 14:07:21 +0300

    fix: Only sending logs to posthog in production environment
    I'm also using pino pretty if NODE_ENV is test

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -59,7 +59,7 @@ const pinoLogger = pino({
     err: pino.stdSerializers.err,
     error: pino.stdSerializers.err,
   },
-  transport: env.NODE_ENV === "development"
+  transport: env.NODE_ENV === "development" || env.NODE_ENV === 'test'
     ? {
         target: "pino-pretty",
         options: {
@@ -75,19 +75,19 @@ const pinoLogger = pino({
 const logger: Logger = {
   fatal(message: string, context?: LogContext) {
     pinoLogger.fatal(context, message);
-    if (env.NODE_ENV !== "development") {
+    if (env.NODE_ENV === 'production') {
       postHogLogger.sendEvent(PostHogEventTypes.ERROR, message, {context});
     }
   },
   error(message: string, context?: LogContext) {
     pinoLogger.error(context, message);
-    if (env.NODE_ENV !== "development") {
+    if (env.NODE_ENV === "production") {
       postHogLogger.sendEvent(PostHogEventTypes.ERROR, message, {context});
     }
   },
   warn(message: string, context?: LogContext) {
     pinoLogger.warn(context, message);
-    if (env.NODE_ENV !== "development") {
+    if (env.NODE_ENV === "production") {
       postHogLogger.sendEvent(PostHogEventTypes.WARNING, message, {context});
     }
   },

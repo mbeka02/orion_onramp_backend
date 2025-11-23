5611e74aec0ba2435bc251bdcab8b1114a75d80d
Patrick Ojiambo <pashrick237@gmail.com>
2025-11-23 11:35:13 +0300

    feat: admin table schema

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -275,3 +275,10 @@ export const treasuryBalanceTable = pgTable("treasury_token_balances", {
   decimals: integer("token_decimals").notNull(),
   balance: bigint("balance", { mode: "bigint" }).notNull()
 })
+export const admin = pgTable("admin", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  name: text("name").notNull(),
+  email: text("email").notNull().unique(),
+  passwordHash: text("password_hash").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+})

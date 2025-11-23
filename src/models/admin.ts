9b69c6990765df5b8e4154c967733bb9847ad6c4
Patrick Ojiambo <pashrick237@gmail.com>
2025-11-23 12:56:27 +0300

    feat: admin auth models

---
+++ src/models/admin.ts
@@ -0,0 +1,59 @@
+import { db } from "../lib/db";
+import { admin } from "../lib/db/schema";
+import { CreateAdminInput, LoginAdminInput } from "../types/admin";
+import { MyError, Errors } from "../errors";
+import { eq } from "drizzle-orm";
+import bcrypt from "bcrypt";
+export class AdminModel {
+    async createAdmin(args: CreateAdminInput) {
+        try {
+            const doesAdminExist = await this.doesAdminExist(args.email);
+            if (doesAdminExist) {
+                throw new MyError(Errors.ADMIN_ALREADY_EXISTS);
+            }
+            const newAdmin = await db.insert(admin).values({
+                name: args.name,
+                email: args.email,
+                password: args.password,
+            }).returning();
+            return newAdmin[0];
+        }
+        catch (error) {
+            throw error;
+        }
+    }
+    async login(args: LoginAdminInput) {
+        try {
+            const admin = await this.getAdminByEmail(args.email);
+            if (!admin) {
+                throw new MyError(Errors.WRONG_ADMIN_CREDENTIALS);
+            }
+            const isPasswordValid = bcrypt.compareSync(args.password, admin.password);
+            if (!isPasswordValid) {
+                throw new MyError(Errors.WRONG_ADMIN_CREDENTIALS);
+            }
+            return admin;
+        }
+        catch (error) {
+            throw error;
+        }
+    }
+    private async doesAdminExist(email: string) {
+        try {
+            const existingAdmin = await db.select().from(admin).where(eq(admin.email, email));
+            return existingAdmin.length > 0;
+        }
+        catch (error) {
+            throw error;
+        }
+    }
+    async getAdminByEmail(email: string) {
+        try {
+            const adminRecord = await db.select().from(admin).where(eq(admin.email, email));
+            return adminRecord[0];
+        }
+        catch (error) {
+            throw error;
+        }
+    }
+}

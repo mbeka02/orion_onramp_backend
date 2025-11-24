5af1a92965b6725aff14718ecd4bd3afa5335ff2
mbeka02 <antonymbeka@gmail.com>
2025-11-24 23:36:53 +0300

    new mock

--- tests/mocks/transaction_model_mock.ts
+++ tests/mocks/transaction_model_mock.ts
@@ -5,5 +5,6 @@ export const transactionModelMock = {
   updateTransactionWithPaystackResponse: jest.fn(),
   updateTransactionStatus: jest.fn(),
   getTransactionByReference: jest.fn(),
+  getTransactionsByEnvironment: jest.fn(),
   getTransactionById: jest.fn(),
 } as TransactionModel;

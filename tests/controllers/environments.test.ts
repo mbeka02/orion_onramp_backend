25a144dc1616747e1293f7100fea97e787efafb2
Roman Njoroge <roman.njoroge@njuguna.com>
2025-12-11 17:23:02 +0300

    test: All tests for environment

--- tests/controllers/environments.test.ts
+++ tests/controllers/environments.test.ts
@@ -970,7 +970,7 @@ describe("Environment Controller: Send Test Webhook", () => {
       expect(false).toBe(true);
     } catch (err) {
       if (err instanceof MyError) {
-        if (err.message === Errors.WEBHOOK_URL_NOT_SET) {
+        if (err.message === Errors.ENVIRONMENT_NOT_FOUND) {
           expect(true).toBe(true);
         }
       } else {
